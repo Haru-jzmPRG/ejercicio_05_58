3f04d6b959a06e0ac1dcfc1be593d4f3e80ebbb2
Jazmin <programajazminc20@gmail.com>
2025-11-17 18:45:00 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        long num = Long.parseLong(System.console().readLine("Introduzca un número: "));
+
+        double digito = 0;
+        double suma = 0;
+        double contador = 0;
+
+        while (num > 0) {
+            digito = (num % 10);
+            suma += digito;
+            contador++;
+            num /= 10;
+        }
+
+        double media = suma / contador;
+
+        System.out.printf("La media de sus digitos es %.2f", media);
     }
 }
